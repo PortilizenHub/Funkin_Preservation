d7a1f6a55a4e7b0d65310a863075be3805c753bf
PortilizenHub <matthewallanvincent09@gmail.com>
2022-08-18 11:58:37 -0500

    testing failed

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -32,7 +32,6 @@ class MainMenuState extends MusicBeatState
 
 	override function create()
 	{
-
 		#if desktop
 		// Updating Discord Rich Presence
 		DiscordClient.changePresence("In the Menus", null);
@@ -103,13 +102,11 @@ class MainMenuState extends MusicBeatState
 		}
 
 		FlxG.camera.follow(camFollow, null, 0.06);
-
-		var versionShit:FlxText = new FlxText(5, FlxG.height - 18, 0, "FNF Preservation v" + CoolUtil.coolTextFile('version.txt'), 12);
+		
+		// var versionShit:FlxText = new FlxText(5, FlxG.height - 18, 0, "FNF Preservation v" + CoolUtil.coolTextFile('version.txt'), 12);
+		var versionShit:FlxText = new FlxText(5, FlxG.height - 18, 0, "FNF Preservation v" + Application.current.meta.get('version'), 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		versionShit.text.split('[');
-		versionShit.text.split(']');
-		trace(versionShit.text);
 		add(versionShit);
 
 		NGio.logEvent('cool yo');
